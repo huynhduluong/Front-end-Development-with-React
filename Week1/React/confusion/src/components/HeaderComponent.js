f64413a23c4f9e2dd111c53b600e16496fc8e020
huynhduluong <woverine147@gmail.com>
2019-10-17 07:46:18 +0700

    single page application Part 1

--- Week1/React/confusion/src/components/HeaderComponent.js
+++ Week1/React/confusion/src/components/HeaderComponent.js
@@ -25,7 +25,7 @@ class Header extends Component {
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} /> 
                         <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
-                        <Collapse  navbar>
+                        <Collapse navbar>
                             <Nav isOpen={this.state.isNavOpen} navbar>
                                 <NavItem>
                                     <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
