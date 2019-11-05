2e5d517b5c867b0f2be62cf0061f396a095b191a
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-06 05:37:01 +0700

    React Animation Component

--- Week1/React/confusion/src/components/DishdetailComponent.js
+++ Week1/React/confusion/src/components/DishdetailComponent.js
@@ -4,18 +4,21 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({dish}){
 	if (dish != null) {			
 		return(
 		<div className="col-12 col-md-5 m-1">
-			<Card>
-			<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-			<CardBody>
-			<CardTitle>{dish.name}</CardTitle>
-			<CardText> {dish.description} </CardText>
-			</CardBody>
-			</Card>
+			<FadeTransform in transformProps={{exitTransform: 'scale(0.5) translateY(-50%)'}}>
+				<Card>
+					<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText> {dish.description} </CardText>
+					</CardBody>
+				</Card>
+			</FadeTransform>
 		</div>        
 		)
 	}
@@ -28,22 +31,27 @@ function RenderDish({dish}){
 	
 function RenderComment({comments, postComment, dishId}){			
 	if (comments != null) {
-		const comment = comments.map((comments) => {
-		return (
-			<div key={comments.id}>
-				<p>{comments.comment}</p>
-				<p>-- {comments.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))} </p>
-			</div>
-		);
-		});
-
 		return(
-		<div className="col-12 col-md-5 m-1">
-			<h4>Comments</h4>
-			{comment}
-			<CommentForm dishId={dishId} postComment={postComment} />
-		</div>				
-		)
+			<div className="col-12 col-md-5 m-1">
+				<h4>Comments</h4>
+				<ul className="list-unstyled">
+					<Stagger in>
+						{comments.map((comment) => {
+							return (
+								<Fade in>
+									<li key={comment.id}>
+										<p>{comment.comment}</p>
+										<p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
+									</li>
+								</Fade>
+							);
+						})}
+					</Stagger>
+
+				</ul>
+				<CommentForm dishId={dishId} postComment={postComment} />
+			</div>
+		);		
 	}
 	else {
 		return(
