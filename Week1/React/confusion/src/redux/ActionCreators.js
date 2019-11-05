6496a72ca5c9cb7290034e398cdf484a95ddb1ac
Huynh Du Luong <huynhduluong@gmail.com>
2019-11-05 20:54:49 +0700

    React Animations

--- Week1/React/confusion/src/redux/ActionCreators.js
+++ Week1/React/confusion/src/redux/ActionCreators.js
@@ -1,5 +1,4 @@
 import * as ActionTypes from './ActionTypes'; // import everything they add all the exports from ActionTypes
-import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (comment) => ({
